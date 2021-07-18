7966d47860df6c274cfbfd46a5cc150b4316b8fd
tdavidi10 <79422317+tdavidi10@users.noreply.github.com>
2021-07-19 00:18:26 +0300

    Whatsapp  Bot
    just change th number of receiver where the ******* are

---
+++ WhatsApp bot.py
@@ -0,0 +1,16 @@
+from twilio.rest import Client
+
+def send(body_text, to_who = "+97252*******"):
+    account_sid = 'AC7211f042a2ea4004fbf253955c411808'
+    auth_token = '2757eda98c8f66cb7f1a217b2af9cc5a'
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body=body_text,
+        to='whatsapp:' + to_who
+    )
+    print(message)
+
+for i in range(1):
+    send("פרטי האשראי נקלטו תודה על ביצוע הרכישה", '+97252*******')
