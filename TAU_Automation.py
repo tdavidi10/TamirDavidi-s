8c42f6bbbf0590b28b5d5c04f7351fa76b5b8e70
Tamir Davidi <79422317+tdavidi10@users.noreply.github.com>
2022-09-09 17:04:15 +0300

    Add files via upload
    insert to code your userName, id & password and put this file in the same folder with the driver

---
+++ TAU_Automation.py
@@ -0,0 +1,61 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.webdriver.common.action_chains import ActionChains
+
+def sleep():
+    time.sleep(0.5)
+
+directory = os.getcwd()
+path = directory + "\chromedriver.exe"
+print(path)
+
+driver = webdriver.Chrome(path)
+driver.maximize_window()
+
+# go to TAU personal info
+driver.get('https://www.ims.tau.ac.il/Tal/Sys/Main.aspx?id=315852608&src=&sys=tal&rightmj=1&dt=08092022193314')
+sleep()
+# eneter username
+user_name = driver.find_element_by_name("txtUser")
+user_name.send_keys("tamirdavidi1") ########################### insert  username
+# enter id
+id = driver.find_element_by_name("txtId")
+id.send_keys("315852608") ############################### insert id
+# enter password
+password = driver.find_element_by_name("txtPass")
+password.send_keys("5g#*CLTrwX^M") ############################### insert password
+# press submit
+enter = driver.find_element_by_name("enter")
+enter.send_keys(Keys.RETURN)
+
+sleep()
+# press notebooks li
+notebooks = driver.find_element_by_id("li7")
+notebooks.click()
+sleep()
+
+# press ishur
+# click tab then enter on the page
+actions = ActionChains(driver)
+actions.send_keys(Keys.TAB)
+actions.send_keys(Keys.RETURN)
+actions.perform()
+
+sleep()
+
+# semster B
+actions = ActionChains(driver)
+
+actions.send_keys(Keys.TAB)
+actions.send_keys(Keys.TAB)
+actions.send_keys(Keys.TAB)
+actions.send_keys(Keys.RETURN)
+actions.perform()
+sleep()
+
+
+
+#time.sleep()
+#driver.close()
