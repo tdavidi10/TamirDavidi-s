7b9a35b7658af343e253e6e9663bc9aadf8ab311
Tamir Davidi <79422317+tdavidi10@users.noreply.github.com>
2024-03-11 22:45:27 +0200

    TAU automation update

--- TAU_Automation.py
+++ TAU_Automation.py
@@ -1,5 +1,6 @@
 import os
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 import time
 from selenium.webdriver.common.action_chains import ActionChains
@@ -7,32 +8,29 @@ from selenium.webdriver.common.action_chains import ActionChains
 def sleep():
     time.sleep(0.5)
 
-directory = os.getcwd()
-path = directory + "\chromedriver.exe"
-print(path)
-
-driver = webdriver.Chrome(path)
-driver.maximize_window()
+driver = webdriver.Chrome(ChromeDriverManager().install())
+#driver.maximize_window()
 
 # go to TAU personal info
 driver.get('https://www.ims.tau.ac.il/Tal/Sys/Main.aspx?id=315852608&src=&sys=tal&rightmj=1&dt=08092022193314')
-sleep()
+sleep()  
 # eneter username
-user_name = driver.find_element_by_name("txtUser")
-user_name.send_keys("insert  username") ########################### insert  username
+user_name = driver.find_element("name", "txtUser")
+user_name.send_keys("########## INSERT USERNAME ##############") ########################### insert  username
+print(user_name)
 # enter id
-id = driver.find_element_by_name("txtId")
-id.send_keys("insert id") ############################### insert id
+id = driver.find_element("name", "txtId")
+id.send_keys("##############INSERT ID ##############") ############################### insert id
 # enter password
-password = driver.find_element_by_name("txtPass")
-password.send_keys("insert password") ############################### insert password
+password = driver.find_element("name", "txtPass")
+password.send_keys("############## INSERT PASS ##############") ############################### insert password 
 # press submit
-enter = driver.find_element_by_name("enter")
+enter = driver.find_element("name", "enter")
 enter.send_keys(Keys.RETURN)
 
 sleep()
 # press notebooks li
-notebooks = driver.find_element_by_id("li7")
+notebooks = driver.find_element("id" ,"li8")
 notebooks.click()
 sleep()
 
@@ -43,19 +41,4 @@ actions.send_keys(Keys.TAB)
 actions.send_keys(Keys.RETURN)
 actions.perform()
 
-sleep()
-
-# semster B
-actions = ActionChains(driver)
-
-actions.send_keys(Keys.TAB)
-actions.send_keys(Keys.TAB)
-actions.send_keys(Keys.TAB)
-actions.send_keys(Keys.RETURN)
-actions.perform()
-sleep()
-
-
-
-#time.sleep()
 #driver.close()
